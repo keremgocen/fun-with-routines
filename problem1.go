0e464712f69905e3b45f601c0f65e329d07ad585
Kerem <kerem@arch.localdomain>
2019-03-24 02:50:52 +0300

    create channel to store problem1 output

--- problem1.go
+++ problem1.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 )
 
-func problem1() {
+func problem1() int {
 
 	log.Printf("problem1: started --------------------------------------------")
 
@@ -14,6 +14,7 @@ func problem1() {
 	// go routines to make sure 100 jobs in total will be
 	// processed despite the number of loops or go routines
 	jobs := make(chan int, 100)
+	numbers := make(chan float32, 100)
 	for j := 1; j <= 100; j++ {
 		jobs <- j
 	}
@@ -28,16 +29,24 @@ func problem1() {
 	wg.Add(100)
 
 	for inx := 0; inx < 10; inx++ {
-		go printRandom1(inx, &wg, jobs)
+		go printRandom1(inx, &wg, jobs, numbers)
 	}
 
 	// wait group will wait for all jobs to be finished
 	wg.Wait()
+	close(numbers)
+
+	nums := make([]float32, 0)
+	for n := range numbers {
+		nums = append(nums, n)
+	}
 
 	log.Printf("problem1: finished --------------------------------------------")
+
+	return len(nums)
 }
 
-func printRandom1(slot int, wg *sync.WaitGroup, jobs <-chan int) {
+func printRandom1(slot int, wg *sync.WaitGroup, jobs <-chan int, nums chan<- float32) {
 
 	//
 	// Do not change 25 into 10!
@@ -47,7 +56,9 @@ func printRandom1(slot int, wg *sync.WaitGroup, jobs <-chan int) {
 		select {
 		case j, ok := <-jobs:
 			if ok {
-				log.Printf("problem1: job=%d slot=%03d count=%05d rand=%f", j, slot, inx, rand.Float32())
+				r := rand.Float32()
+				log.Printf("problem1: job=%d slot=%03d count=%05d rand=%f", j, slot, inx, r)
+				nums <- r
 				wg.Done()
 			} else {
 				log.Printf("channel closed at slot=%03d", slot)
